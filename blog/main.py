3b437cabe897156a1b27a0d7e96c1b9f26d703ca
dbmurali <84655388+dbmurali@users.noreply.github.com>
2025-02-14 21:34:08 +0530

    Update main.py
    Replaced contact info storage in text file with SMTP email sending

--- blog/main.py
+++ blog/main.py
@@ -1,9 +1,7 @@
-import json
 from datetime import datetime as dt
-from flask import Flask, render_template,request
-
-import requests
-
+from flask import Flask, render_template, request, send_from_directory
+import os,requests
+from smtplib import  SMTP
 
 response=requests.get("https://api.npoint.io/d4656264c8ac08d072a5")
 
@@ -18,6 +16,7 @@ app=Flask(__name__)
 def home():
     
     return render_template("index.html",post=res, date=today)
+
 
 @app.route("/<page>")
 def pages(page):
@@ -40,10 +39,22 @@ def contact():
            "phone_number":request.form["PHONE_NUMBER"],
            "message":request.form["MSG"]
        }
-   with open("contact.txt","a") as file:
-    file.write(str(contacts)+"\n")
+   sender_email = "dbmurali1507@gmail.com"
+   app_pass = os.environ.get("APP_PASSWORD")
+   to_mail="areadb15@gmail.com"
+   viewer_email=contacts["e-mail"]
+   viewer_name=contacts["name"]
+   viewer_phone=contacts["phone_number"]
+   viewer_msg=contacts["message"]
 
-    return "<h1>FORM SUBMITTED</h1>"
+   server=SMTP("smtp.gmail.com", 587)
+   server.starttls()
+   server.login(user=sender_email,password=app_pass)
+
+   server.sendmail(from_addr= sender_email,to_addrs=to_mail,msg=f"Subject:Message From {viewer_name} (my_blog)\n\n{viewer_msg}\n\n\n Contact details:\n e-mail: {viewer_email}\n contact number: {viewer_phone}")
+   print("Mail send")
+
+   return "<h1>FORM SUBMITTED</h1>"
 
 
 
